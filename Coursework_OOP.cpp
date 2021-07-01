b5757bae49cf4bd2ee5ce3a0aaae5f591d0ee8a3
Евгений <rabota142@inbox.ru>
2021-07-01 22:22:13 +0300

    Update
    Separation of access rights.
    Using the friend class.
    Change: rules for naming variables (Diagonal - > diagonal).

--- Coursework_OOP.cpp
+++ Coursework_OOP.cpp
@@ -6,135 +6,132 @@ using namespace std;
 class System_unit // Клас "Системный блок"
 {
 public:
-    string CPU; // Модель процессора
-    double Clock_frequency; // Тактовая часота
-    double Hard_drive_size; // Емкость жесткого диска
-    string CD_drive; // Модель CD-привода
-    double Price_S; // Стоимость
-
     System_unit() // Конструктор класса
     {
             cout << "Введіть модель процессору\n";           
-            cin >> CPU;                               
+            cin >> cpu;                               
             cout << "Введіть тактову частоту\n";         
-            cin >> Clock_frequency;                    
+            cin >> clock_frequency;                    
             cout << "Введіть ємність жорсткого диска\n";          
-            cin >> Hard_drive_size;                    
+            cin >> hard_drive_size;                    
             cout << "Введіть модель СD-приводу\n";            
-            cin >> CD_drive;                           
+            cin >> cd_drive;                           
             cout << "Введіть вартість\n";                    
-            cin >> Price_S;                            
+            cin >> price_s;                            
     }
+    friend class Computer;
+private:
+    string cpu; // Модель процессора
+    double clock_frequency; // Тактовая часота
+    double hard_drive_size; // Емкость жесткого диска
+    string cd_drive; // Модель CD-привода
+    double price_s; // Стоимость
 };
 
 
 class Monitor // Класс "Монитор"
 {
 public:
-    double Diagonal; // Диагональ
-    string Screen_resolution; // Разрешение экрана
-    double Price_M; // Стоимость
-
     Monitor() // Конструктор класса
     {
         cout << "Введіть діагональ\n";                    
-        cin >> Diagonal;                              
+        cin >> diagonal;                              
         cout << "Введіть роздільну здатність екрану\n";           
-        cin >> Screen_resolution;                    
+        cin >> screen_resolution;                    
         cout << "Введіть вартість\n";                       
-        cin >> Price_M;                                
+        cin >> price_m;                                
     }
+    friend class Computer;
+private:
+    double diagonal; // Диагональ
+    string screen_resolution; // Разрешение экрана
+    double price_m; // Стоимость
 };
 
 class Keyboard // Класс "Клавиатура"
 {
 public:
-    string K_type; // Модель клавиатуры
-    int Number_of_keys; // Количество клавиш
-    double Price_K; // Стоимость
-
     Keyboard() // Конструктор класса
     {
         cout << "Введіть модель клавіатури\n";             
-        cin >> K_type;                                
+        cin >> k_type;                                
         cout << "Введіть кількість клавіш\n";              
-        cin >> Number_of_keys;                        
+        cin >> number_of_keys;                        
         cout << "Введіть вартість\n";                      
-        cin >> Price_K;                               
+        cin >> price_k;                               
     }
+    friend class Computer;
+private:
+    string k_type; // Модель клавиатуры
+    int number_of_keys; // Количество клавиш
+    double price_k; // Стоимость
 };
 
 class Mouse // Класс "Мышь"
 {
 public:
-    string M_type; // Тип мыши
-    int Number_of_buttons; // Количество кнопок
-    double Price_Ms; // Стоимость
-
     Mouse()
     {
         cout << "Введіть модель миші\n";                 
-        cin >> M_type;                                
+        cin >> m_type;                                
         cout << "Введіть кількість кнопок\n";           
-        cin >> Number_of_buttons;                     
+        cin >> number_of_buttons;                     
         cout << "Введіть вартість\n";                       
-        cin >> Price_Ms;                              
+        cin >> price_ms;                              
     }
+    friend class Computer;
+private:
+    string m_type; // Тип мыши
+    int number_of_buttons; // Количество кнопок
+    double price_ms; // Стоимость
 };
 
 
 class Computer: System_unit, Monitor, Keyboard, Mouse // Класс "Компьютер". Наследуеться от всех вышеописанных классов
 {
 public:
-
-    double Price; // Стоимость
-
     Computer()
     {
-        Price = Price_S + Price_M + Price_K + Price_Ms; // Подсчет итоговой стоимости компьютера
+        price = price_s + price_m + price_k + price_ms; // Подсчет итоговой стоимости компьютера
     }
-
     void GetInfo() // Функция для вывода информации про объект класса
     {
-        cout << "Модель процессору - " << CPU << "\n";
-        cout << "Ємність жорсткого диска - " << Hard_drive_size << "\n";
-        cout << "Модель CD-приводу - " << CD_drive << "\n";
-        cout << "Такстова частота - " << Clock_frequency << "\n";
-        cout << "Вартість системного блоку - " << Price_S << "\n";
-        cout << "Діагональ монітору - " << Diagonal << "\n";
-        cout << "Роздільна здатність екрану - " << Screen_resolution << "\n";
-        cout << "Вартість монітору - " << Price_M << "\n";
-        cout << "Модель клавіатури - " << K_type << "\n";
-        cout << "Кількість клавіш - " << Number_of_keys << "\n";
-        cout << "Вартість клавіатури - " << Price_K << "\n";
-        cout << "Модель миші - " << M_type << "\n";
-        cout << "Кількість кнопок миші - " << Number_of_buttons << "\n";
-        cout << "Вартість миші - " << Price_Ms << "\n";
+        cout << "Модель процессору - " << cpu << "\n";
+        cout << "Ємність жорсткого диска - " << hard_drive_size << "\n";
+        cout << "Модель CD-приводу - " << cd_drive << "\n";
+        cout << "Такстова частота - " << clock_frequency << "\n";
+        cout << "Вартість системного блоку - " << price_s << "\n";
+        cout << "Діагональ монітору - " << diagonal << "\n";
+        cout << "Роздільна здатність екрану - " << screen_resolution << "\n";
+        cout << "Вартість монітору - " << price_m << "\n";
+        cout << "Модель клавіатури - " << k_type << "\n";
+        cout << "Кількість клавіш - " << number_of_keys << "\n";
+        cout << "Вартість клавіатури - " << price_k << "\n";
+        cout << "Модель миші - " << m_type << "\n";
+        cout << "Кількість кнопок миші - " << number_of_buttons << "\n";
+        cout << "Вартість миші - " << price_ms << "\n";
         cout << "******************************";
     }
-
     void GetPrice() // Функция вывода стоимости компьютера
     {
-        cout << "Загальна вартість = " << Price <<"\n"; // Вывод стоимости
+        cout << "Загальна вартість = " << price <<"\n"; // Вывод стоимости
     }
+    friend class Computer_class;
+private:
+    double price;
 };
 
 class Computer_class // Класс "Компьютерный класс"
 {
 public:
-
-    double All_price; // Итоговая стоимость
-    Computer arr[3]; // Массив объектов класса "Компьютер"
-
     Computer_class()
     {   double all = 0;
         for (int i = 0; i < 3; i++)
         {
-            all = all + arr[i].Price; // Подсчет итоговой стоимости
+            all = all + arr[i].price; // Подсчет итоговой стоимости
         }
-         All_price = all; 
+        All_price = all; 
     }
-
     void GetAllInfo() // Функция для получения информации про все компьютеры в классе
     {
         for (int i = 0; i < 3; i++)
@@ -143,7 +140,6 @@ public:
             arr[i].GetInfo(); // Вызов функции для вывода информации про компьютер
         }
     }
-
     void GetOnePrice() // Функция для вывода стоимости компьютеров по отдельности
     {
         for (int i = 0; i < 3; i++)
@@ -152,11 +148,13 @@ public:
             arr[i].GetPrice(); // Вызов функции для вывода стоимости одного компьютера
         }
     }
-
     void GetAllPrice() // Функция для вывода суммарной стоимости оборудования компьютерного класса 
     {
         cout << "Загальна вартість обладнання класу - " << All_price <<"\n"; // Вывод суммарной стоимости всех компьютеров
     }
+private:
+    double All_price; // Итоговая стоимость
+    Computer arr[3]; // Массив объектов класса "Компьютер"
 };
 
 int main()
